31909eb2f5f6b510d2767f50d72065ae3039a681
Robert Yee <Ryee002@ucr.edu>
2018-12-05 00:49:20 -0800

    Updated all these files with login/signup

--- RideU.py
+++ RideU.py
@@ -1,10 +1,26 @@
 from flask import Flask, render_template, url_for, redirect, flash
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
 from forms import SignUpForm, LoginForm
 app = Flask(__name__)
 
 
 # Secret key helps to keep the site secure
 app.config['SECRET_KEY'] = 'RDSS2134C#++-ABC'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    # posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
 
 @app.route("/")
@@ -32,6 +48,11 @@ def login():
         else:
             flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
+
+
+@app.route("/biography")
+def bio():
+    return render_template('bio.html', title="Biography")
 
 
 if __name__ == '__main__':
